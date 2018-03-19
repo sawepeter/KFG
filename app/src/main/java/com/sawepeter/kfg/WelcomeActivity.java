6fe171134cbc0a8a6e75019dd9e114d5d90465bd
user.email <sawepeter6@gmail.com>
2018-03-19 17:02:21 +0300

    welcome screen

--- app/src/main/java/com/sawepeter/kfg/WelcomeActivity.java
+++ app/src/main/java/com/sawepeter/kfg/WelcomeActivity.java
@@ -1,13 +1,28 @@
 package com.sawepeter.kfg;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotslayout;
+    private TextView[] dots;
+    private Button btnSkip, btnNext;
+    private PrefManager prefManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //checking for first time launch-  before calling setcontentView()
+        prefManager = new PrefManager(this);
         setContentView(R.layout.activity_welcome);
     }
 }
