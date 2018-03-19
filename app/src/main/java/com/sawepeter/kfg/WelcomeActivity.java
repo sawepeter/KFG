09e2657ecc0defc6fbee8f9e93687252dc2e09ec
user.email <sawepeter6@gmail.com>
2018-03-19 23:23:46 +0300

    event handlers

--- app/src/main/java/com/sawepeter/kfg/WelcomeActivity.java
+++ app/src/main/java/com/sawepeter/kfg/WelcomeActivity.java
@@ -44,6 +44,43 @@ public class WelcomeActivity extends AppCompatActivity {
 
 
         //layouts of all welcome sliders
-        
+        layouts = new int[]{
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2,
+                R.layout.welcome_slide3,
+                R.layout.welcome_slide4};
+
+        //adding bottom dots
+        addBottomDots(0);
+
+        //making notification bar transparent
+        changeStatusBarColor();
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchHomeScreen();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //checking for the last page
+                //if last page home screen will be launched
+                int current = getItem(+1);
+                if (current  < layouts.length) {
+                    //move to next screen
+                    viewPager.setCurrentItem(current);
+            } else {
+                    launchHomeScreen();
+                }
+        });
+        }
     }
 }
