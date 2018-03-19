109a70fde7c8a49cb992587997fa79f6b5ce210e
user.email <sawepeter6@gmail.com>
2018-03-19 14:22:01 +0300

    prefernce class

--- app/src/main/java/PrefManager.java
+++ app/src/main/java/PrefManager.java
@@ -19,6 +19,18 @@ public class PrefManager {
     private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
 
     public PrefManager(Context context){
-        
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public  void setIsFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch(){
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
     }
 }
+
